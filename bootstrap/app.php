aaaf6bf5f810f146c4588eb1565ccac55aef6857
joounii <jonifu06@gmail.com>
2026-04-23 13:43:54 +0200

    added link headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AddLinkHeaders;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->web(append: [
+            AddLinkHeaders::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
